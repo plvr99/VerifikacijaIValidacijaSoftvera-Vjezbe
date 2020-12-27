2105f210f13c96da55b6b293bf4197dfc98f0f76
Ehlimana <ekrupalija1@etf.unsa.ba>
2020-12-27 01:43:52 +0100

    Commit programskog koda

---
+++ FlightControlApp/FlightControlApp/Data/FlightControlAppContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using FlightControlApp.Models;
+
+namespace FlightControlApp.Data
+{
+    public class FlightControlAppContext : DbContext
+    {
+        public FlightControlAppContext (DbContextOptions<FlightControlAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<FlightControlApp.Models.Let> Let { get; set; }
+    }
+}
